17244f040105a8f066871040800a00e5e82895a3
Jose Cabrera <jcabrera9409@gmail.com>
2025-07-23 15:45:20 -0500

    updating class name

--- monolito/src/main/java/com/surrender/model/DetalleRecibo.java
+++ monolito/src/main/java/com/surrender/model/DetalleRecibo.java
@@ -9,7 +9,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tblDetalleContrato")
+@Table(name = "tblDetalleRecibo")
 public class DetalleRecibo {
 
 	@Id
