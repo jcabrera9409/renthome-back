6501ad5ca1de00e79ddce47fc7e0bc0fceb2b869
Jose Cabrera <jcabrera9409@gmail.com>
2025-08-16 18:34:27 -0500

    adding modal for contrato

---
+++ front/src/app/modals/contrato-edicion-dialog/contrato-edicion-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contrato-edicion-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './contrato-edicion-dialog.component.html',
+  styleUrl: './contrato-edicion-dialog.component.css'
+})
+export class ContratoEdicionDialogComponent {
+
+}
--- front/src/app/pages/contrato/contrato.component.ts
+++ front/src/app/pages/contrato/contrato.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { LucideAngularModule, PlusIcon } from 'lucide-angular';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { ContratoEdicionDialogComponent } from '../../modals/contrato-edicion-dialog/contrato-edicion-dialog.component';
 
 @Component({
   selector: 'app-contrato',
   standalone: true,
-  imports: [LucideAngularModule],
+  imports: [LucideAngularModule, MatDialogModule],
   templateUrl: './contrato.component.html',
   styleUrl: './contrato.component.css'
 })
 export class ContratoComponent {
   readonly plusIcon = PlusIcon;
+
+  constructor(private dialog: MatDialog) {}
+
+  openDialog() {
+    const dialogRef = this.dialog.open(ContratoEdicionDialogComponent, {
+      data: { /* Pass data to the dialog if needed */ },
+      panelClass: 'w-full'
+    });
+  }
 }
